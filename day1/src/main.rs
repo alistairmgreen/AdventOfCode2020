133fe3ac1308bfa5067af76b863dcd2c9a240ce9
Alistair Green <alistairmgreen@gmail.com>
2020-12-01 23:27:37 +0000

    Day 1, part 2.

--- day1/src/main.rs
+++ day1/src/main.rs
@@ -13,16 +13,23 @@ fn main() {
             println!("{} * {} = {}", x, y, x * y);
         }
         None => {
-            println!("No solution found.");
+            println!("No solution found for part 1.");
+        }
+    }
+
+    for (index, &n) in numbers[..numbers.len() - 1].iter().enumerate() {
+        let required = 2020 - n;
+        if let Some((x, y)) = pair_adding_to(&numbers[index + 1..], required) {
+            println!("{} + {} + {} = 2020", n, x, y);
+            println!("{} * {} * {} = {}", n, x, y, n * x * y);
         }
     }
 }
 
 fn pair_adding_to(numbers: &[i32], target: i32) -> Option<(i32, i32)> {
-    let max_index = numbers.len() - 1;
-    for (index, &n) in numbers.iter().enumerate() {
+    for (index, &n) in numbers[..numbers.len() - 1].iter().enumerate() {
         let required = target - n;
-        if index < max_index && numbers[index+1..].binary_search(&required).is_ok() {
+        if numbers[index + 1..].binary_search(&required).is_ok() {
             return Some((n, required));
         }
     }
