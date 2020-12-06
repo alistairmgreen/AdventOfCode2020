be6dada91990ead87a255560c0db9cf7be5e3493
Alistair Green <alistairmgreen@gmail.com>
2020-12-06 11:45:23 +0000

    Day 6, part 2.

--- day6/src/main.rs
+++ day6/src/main.rs
@@ -2,28 +2,50 @@ use itertools::Itertools;
 use std::collections::HashSet;
 
 fn main() {
-    let sum_counts: usize = include_str!("puzzle_input.txt")
+    let groups: Vec<Vec<&str>> = include_str!("puzzle_input.txt")
         .lines()
         .batching(read_group)
-        .map(|group| group.len())
-        .sum();
+        .collect();
 
-    println!("The sum of counts is {}.", sum_counts);
+    let part1: usize = groups.iter().map(|group| anyone_answered(group)).sum();
+
+    println!("Part 1: The sum of counts is {}.", part1);
+
+    let part2: usize = groups.iter().map(|group| everyone_answered(group)).sum();
+
+    println!("Part 2: The sum of counts is {}.", part2);
 }
 
-fn read_group<'a, T>(answers: &mut T) -> Option<HashSet<char>>
+fn read_group<'a, T>(answers: &mut T) -> Option<Vec<&'a str>>
 where
     T: Iterator<Item = &'a str> + Clone,
 {
-    let group: HashSet<char> = answers
-        .take_while_ref(|line| !line.is_empty())
-        .flat_map(|line| line.chars())
-        .collect();
+    let group: Vec<_> = answers.take_while_ref(|line| !line.is_empty()).collect();
     answers.next();
 
     if group.is_empty() {
         None
     } else {
         Some(group)
+    }
+}
+
+fn anyone_answered(group: &[&str]) -> usize {
+    group
+        .iter()
+        .flat_map(|line| line.chars())
+        .collect::<HashSet<char>>()
+        .len()
+}
+
+fn everyone_answered(group: &[&str]) -> usize {
+    let answers = group
+        .iter()
+        .map(|line| line.chars().collect::<HashSet<char>>())
+        .fold1(|a, b| a.intersection(&b).cloned().collect::<HashSet<char>>());
+    
+    match answers {
+        Some(a) => a.len(),
+        None => 0
     }
 }
