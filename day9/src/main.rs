47bb11d03f36d96dd827ac8db8908ba67ad87df8
Alistair Green <alistairmgreen@gmail.com>
2020-12-09 20:24:17 +0000

    Day 9, part 2.

--- day9/src/main.rs
+++ day9/src/main.rs
@@ -7,6 +7,13 @@ fn main() {
     match find_invalid(&numbers, 25) {
         Some(n) => {
             println!("{} is invalid.", n);
+
+            if let Some(range) = contiguous_summing_to(&numbers, n) {
+                println!("Set of {} contiguous numbers adds up to {}.", range.len(), n);
+                let min = range.iter().min().unwrap();
+                let max = range.iter().max().unwrap();
+                println!("{} + {} = {}", min, max, min + max);
+            }
         }
         None => {
             println!("No solution found for part 1.");
@@ -37,6 +44,19 @@ fn find_invalid(numbers: &[i64], window: usize) -> Option<i64> {
     None
 }
 
+fn contiguous_summing_to(numbers: &[i64], target: i64) -> Option<&[i64]> {
+    for length in 2..=numbers.len() {
+        if let Some(portion) = numbers
+            .windows(length)
+            .find(|&window| window.iter().sum::<i64>() == target)
+        {
+            return Some(portion);
+        }
+    }
+
+    None
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
@@ -49,5 +69,16 @@ mod tests {
         ];
 
         assert_eq!(find_invalid(&numbers, 5), Some(127));
+    }
+
+    #[test]
+    fn test_contiguous_summing_to() {
+        let numbers = vec![
+            35, 20, 15, 25, 47, 40, 62, 55, 65, 95, 102, 117, 150, 182, 127, 219, 299, 277, 309,
+            576,
+        ];
+
+        let result = contiguous_summing_to(&numbers, 127).unwrap();
+        assert_eq!(result, &[15, 25, 47, 40]);
     }
 }
